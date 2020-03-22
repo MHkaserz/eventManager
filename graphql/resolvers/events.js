80c89d9a411576d5d2c638624e48c46ca307dd8d
El-Hassan Mohsen Farid <hassan.m.farid@gmail.com>
2020-03-22 21:32:18 +0100

    Updated events.js

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -8,38 +8,38 @@ const User = require('../../models/user');
 // rootValue
 module.exports = {
   	events: async () => {
-    	try {
-    		const events = await Event.find();
-    		return events.map(event => { return transformEvent(event); });
-    	} catch (err) { throw err; }
+      	try {
+        		const events = await Event.find();
+        		return events.map(event => { return transformEvent(event); });
+      	} catch (err) { throw err; }
   	},
 
   	createEvent: async args => {
-    	// Create the event
-		const event = new Event({
+    	  // Create the event
+		    const event = new Event({
 
-	  		title:        args.eventInput.title,
-	  		description:  args.eventInput.description,
-	  		price:        +args.eventInput.price,
-	  		date:         new Date(args.eventInput.date),
-	  		category:     args.eventInput.category,
-	  		owner:        '5e7576e2a848053c5c97a65b' // TODO: get the owner dynamically
+    	  		title:        args.eventInput.title,
+    	  		description:  args.eventInput.description,
+    	  		price:        +args.eventInput.price,
+    	  		date:         new Date(args.eventInput.date),
+    	  		category:     args.eventInput.category,
+    	  		owner:        '5e7576e2a848053c5c97a65b' // TODO: get the owner dynamically
 
-		}); 
+		    }); 
 
-    	let createdEvent;
+    	  let createdEvent;
 
-    	try {
-    		// Send the event to the Database, then add it to its owner in Users
-    		const result = await event.save();
-    		createdEvent = transformEvent(result);
+      	try {
+      		  // Send the event to the Database, then add it to its owner in Users
+      		  const result = await event.save();
+      		  createdEvent = transformEvent(result);
 
-    		const owner = await User.findById('5e7576e2a848053c5c97a65b'); // TODO: get the owner dynamically
+      		  const owner = await User.findById('5e7576e2a848053c5c97a65b'); // TODO: get the owner dynamically
 
-			if (!owner) { throw new Error('Owner doesn\'t exist'); }
-			owner.ownes.push(event);
-			await owner.save();
-			return createdEvent;
-    	} catch (err) { throw err; }
+  			    if (!owner) { throw new Error('User doesn\'t exist'); }
+  			    owner.ownes.push(event);
+  			    await owner.save();
+  			    return createdEvent;
+      	} catch (err) { throw err; }
   	}
 };
